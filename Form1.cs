2dba91e334a4cfd440992bd79d00956f286c890b
VinCal <vincentcallebaut@hotmail.com>
2015-05-03 22:58:15 +0100

    First Commit

---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Autodesk.Max;
+
+namespace ExternalMaxscriptListener
+{
+    /// <summary>
+    /// Autodesk.Max Plugin class, gets loaded when Max loads. Hide the form and disable ShowInTaskbar.
+    /// </summary>
+    public class ExternalMaxscriptListner : IPlugin
+    {
+        public void Cleanup()
+        { }
+
+        public void Initialize(IGlobal global, ISynchronizeInvoke sync)
+        {
+            var dialog = new ExternalMaxscriptListener();
+            dialog.Show();
+            dialog.Hide();
+            dialog.ShowInTaskbar = false;
+        }
+    }
+
+    /// <summary>
+    /// ExternalMaxscriptListener is only used to send messages to from any other process and it will then Evaluate the file or Execute the maxscript command
+    /// </summary>
+    public class ExternalMaxscriptListener : Form
+    {
+        private const int WM_USER = 0x0400;
+        private const int WM_USER_INITIALIZE = WM_USER + 0x8000;
+        private const int WM_COPYDATA = 0x4A;
+
+        //Used for WM_COPYDATA for string messages
+        [StructLayout(LayoutKind.Sequential)]
+        public struct COPYDATASTRUCT
+        {
+            public IntPtr dwData;
+            public int cbData;
+            public IntPtr lpData;
+        }
+
+        public ExternalMaxscriptListener()
+        {
+            ClientSize = new System.Drawing.Size(1, 1);
+            Name = "MxsListener";
+            Text = "External Maxscript Listener";
+        }
+        
+        protected override void WndProc(ref Message m)
+        {
+            switch (m.Msg)
+            {
+                case WM_USER_INITIALIZE:
+                    m.Result = (IntPtr)1;
+                    return;
+
+                case WM_COPYDATA:
+                    var cds = (COPYDATASTRUCT)Marshal.PtrToStructure(m.LParam, typeof(COPYDATASTRUCT)); 
+                    var buff = new byte[cds.cbData];
+                    Marshal.Copy(cds.lpData, buff, 0, cds.cbData);
+                    string msg = Encoding.ASCII.GetString(buff, 0, cds.cbData);
+
+                    if (File.Exists(msg))
+                    {
+                        string errorLog = String.Empty;
+                        bool result = GlobalInterface.Instance.FileinScriptEx(msg, errorLog);
+                        if (!result)
+                        {
+                            Log(errorLog, true);
+                        }
+                    }
+                    else
+                        GlobalInterface.Instance.ExecuteMAXScriptScript(msg, false, null);
+
+                    m.Result = (IntPtr)1;
+                    return;
+            }
+
+            base.WndProc(ref m); 
+        }
+
+        public static void Log(string message, bool newLine)
+        {
+            GlobalInterface.Instance.TheListener.EditStream.Printf(message + (newLine ? Environment.NewLine : string.Empty), null);
+        }
+    }
+}
---
+++ SendMaxscript/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace SendMaxscript
+{
+    class Program
+    {
+        [DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        static extern IntPtr FindWindowByCaption(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("User32.dll", EntryPoint = "SendMessage")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll", SetLastError = true, EntryPoint = "SendMessage")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int Msg, int wParam, ref COPYDATASTRUCT lParam);
+
+        //Used to define private messages for use by private window classes, usually of the form WM_USER+x, where x is an integer value.
+        private const int WM_USER = 0x0400;
+
+        //Message numbers in the third range (0x8000 through 0xBFFF) are available for applications to use as private messages. 
+        //Messages in this range do not conflict with system messages.
+        private const int WM_USER_INITIALIZE = WM_USER + 0x8000;
+
+        //Message used with COPYDATASTRUCT
+        private const int WM_COPYDATA = 0x4A;
+
+        //Used for WM_COPYDATA for string messages
+        [StructLayout(LayoutKind.Sequential)]
+        public struct COPYDATASTRUCT
+        {
+            public IntPtr dwData;
+            public int cbData;
+            public IntPtr lpData;
+        }
+
+        private static IntPtr _ExternalMxsListenerHwnd;
+
+        static void Main(string[] args)
+        {
+            bool result = FindExternalMaxscriptListener();
+
+            if (result)
+            {
+                Console.WriteLine(@"Type: ""exit"" to exit this application.");
+                Console.WriteLine("Enter Maxscript commands:");
+
+                while (true)
+                {
+                    string line = Console.ReadLine(); // Get string from user
+                    if (line == "exit") // Check string
+                        break;
+
+                    //Send Maxscript command or ms file path to the External Maxscript Listener plugin
+                    SendMaxscript(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Unable to find External Maxscript Listener window");
+            }
+        }
+
+        private static bool FindExternalMaxscriptListener()
+        {
+            _ExternalMxsListenerHwnd = FindWindowByCaption(IntPtr.Zero, "External Maxscript Listener");
+            if (_ExternalMxsListenerHwnd != IntPtr.Zero)
+            {
+                //Send initialize message to check if it's our window and not a random one with the exact same name...(unlikely :D)
+                IntPtr resultPtr = SendMessage(_ExternalMxsListenerHwnd, (uint) WM_USER_INITIALIZE, (IntPtr) 1,IntPtr.Zero);
+                int initResult = resultPtr.ToInt32();
+
+                if (initResult == 1) return true;
+            }
+
+            return false;
+        }
+
+        private static void SendMaxscript(string msg)
+        {
+            byte[] buff = Encoding.ASCII.GetBytes(msg);
+
+            COPYDATASTRUCT cds;
+            cds.dwData = (IntPtr)100;
+            cds.lpData = Marshal.AllocHGlobal(buff.Length);
+            Marshal.Copy(buff, 0, cds.lpData, buff.Length);
+            cds.cbData = buff.Length;
+
+            IntPtr stringResult = SendMessage(_ExternalMxsListenerHwnd, WM_COPYDATA, 0, ref cds);
+
+            Marshal.FreeHGlobal(cds.lpData);
+            if (stringResult == IntPtr.Zero)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+    }
+}
